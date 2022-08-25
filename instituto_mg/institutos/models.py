8b428fea44db907baf23445e420f76cecbfeab52
Gica <gquirozpretell@frba.utn.edu.ar>
2022-08-24 22:26:31 -0300

    cambios en models

--- instituto_mg/institutos/models.py
+++ instituto_mg/institutos/models.py
@@ -6,16 +6,25 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.nombre}-{self.camada}"
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=30)
